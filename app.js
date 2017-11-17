14546e343073b95df376f458fc3055e8cd474e86
Derek Kramer <derek.kramer88@gmail.com>
2017-11-17 08:09:10 -0700

    ✅ Testing error in image route

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ function writeImage() {
                 }
                 svg += '</svg>';
             })
-            .then(() => svg2png(svg, { width: 3000, height: 3000 }))
+            .then(() => svg2png(svg, { width: 1000, height: 1000 }))
             .then(buffer => fs.writeFile("quilt.png", buffer, err => {
                 if(err) throw err;
             }))
